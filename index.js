15ea064d15d1f7daca2d90ec1927e483dc453d22
Rez <77873738+rezcoco@users.noreply.github.com>
2022-06-12 02:11:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const qrcode = require('qrcode-terminal');
-
+const express = require('express');
+const PORT = process.env.PORT || 8080
 const { Client } = require('whatsapp-web.js');
-const client = new Client();
+
+const options = [ '--disable-gpu', '--disable-setuid-sandbox', '--no-sandbox', '--no-zygote' ]
+const client = new Client({
+  puppeteer: {
+    headless: true
+    args: options
+  }
+});
+
+const app = express();
+app.listen(PORT, () => console.log(`Running at Port: ${PORT}`);
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+  qrcode.generate(qr, qrcode => {
+    console.log(qrcode)
+    app.get('/', (req, res) => {
+      res.send(qrcode)
+    })
+  });
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+  console.log('Client is ready!');
 });
 
 client.initialize();
