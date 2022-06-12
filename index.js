0ab79e8171d3fa4659fa001fcb2c95a6806e510e
Rez <77873738+rezcoco@users.noreply.github.com>
2022-06-12 08:19:02 +0700

    Create index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const qrcode = require('qrcode');
 const express = require('express');
+const bp = require('body-parser');
 const PORT = process.env.PORT || 8080
 const { Client } = require('whatsapp-web.js');
 
@@ -12,6 +13,8 @@ const client = new Client({
 });
 
 const app = express();
+app.use(bp.urlencoded({ extended: false }));
+app.use(bp.json());
 app.listen(PORT, () => console.log(`Running at Port: ${PORT}`));
 
 client.on('qr', qr => {
