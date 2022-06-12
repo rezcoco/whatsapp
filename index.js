f82a844d8eaeffe54dd345f1f0a16b896a789885
Rez <77873738+rezcoco@users.noreply.github.com>
2022-06-12 10:11:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const qrcode = require('qrcode');
 const express = require('express');
 const bp = require('body-parser');
 const PORT = process.env.PORT || 8080
-const { Client } = require('whatsapp-web.js');
+const { Client, Contact } = require('whatsapp-web.js');
 
 const options = [ '--no-sandbox', '--disable-setuid-sandbox' ]
 const client = new Client({
@@ -11,6 +11,7 @@ const client = new Client({
     args: options 
   }
 });
+const contact = new Contact()
 
 const app = express();
 
@@ -31,5 +32,29 @@ client.on('qr', qr => {
 client.on('ready', () => {
   console.log('Client is ready!');
 });
+
+client.on('message', message => {
+  let msg
+	if(!contact.isMyContact) {
+	  msg = 'Apakah anda kurir ?\nKetik "#ya" jika ya, "#bukan" jika bukan.\n\n"Ini adalah pesan otomatis"'
+		return client.sendMessage(message.from, msg);
+	}
+	if (message.body === '#ya') {
+	  msg = 'Ketik "#paket" jika mau antar paket, "#lokasi" jika anda perlu detail lokasi'
+	  return client.reply(message.from, msg);
+	}
+	if (message.body === '#paket') {
+	  msg = 'Tolong taruh paketnya di kursi / depan pintu kamar kos saya (No 3)\n*NOTE:* Usahakan jangan dititipkan'
+    return client.reply(message.from, msg);
+	}
+	if (message.body === '#lokasi') {
+	  msg = 'https://maps.app.goo.gl/CBAPNRqB8EtCx26E6'
+    return client.reply(message.from, msg);
+	}
+	if (message.body === '#bukan') {
+	  msg = 'Oke, silahkan tuliskan pesan anda'
+    return client.reply(message.from, msg);
+	}
+});
  
-client.initialize();
+client.initialize(); 
